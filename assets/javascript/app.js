7f4f95b28f68c201e14345c9abcb4d74068e6a9f
Hungdz <hungdd0505@gmail.com>
2023-06-06 07:55:20 +0700

    hahahaa

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -31,14 +31,42 @@ $(document).ready(function () {
 $(document).ready(function () {
   $(".listproduct__list-adv").slick({
     dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
   });
 });
 
 $(document).ready(function () {
-  $(".listproduct__container-type-list").slick({
+  $(".type-list").slick({
     slidesToShow: 7,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
   });
 });
+
+$(document).ready(function () {
+  $(".listproduct__container-list-img").slick({
+    dots: true,
+  });
+});
+
+const btnIncrease = document.querySelector(
+  ".basket__main-quantity-btn-increase"
+);
+const btnDecrease = document.querySelector(
+  ".basket__main-quantity-btn-decrease"
+);
+const inputQuantity = document.querySelector(
+  ".basket__main-quantity-box input"
+);
+
+btnIncrease.onclick = () =>
+  (inputQuantity.value = parseInt(inputQuantity.value) + 1);
+
+btnDecrease.onclick = () => {
+  if (inputQuantity.value > 0) {
+    inputQuantity.value = parseInt(inputQuantity.value) - 1;
+  }
+  return;
+};
